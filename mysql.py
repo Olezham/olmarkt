d483ad411ab3d45efde495dd2df5bd2147beccd6
Oleh <64526832+Olezham@users.noreply.github.com>
2024-02-06 22:25:09 +0100

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -1,143 +1,184 @@
 import pymysql
 import bcrypt
 import uuid
-
-import time, datetime
-
-import os
-
+import time
+import datetime
 from config import host, username, password, db
-
 
 current_data = datetime.datetime.now()
 joindata = time.mktime(current_data.timetuple())
-
 salt = bcrypt.gensalt()
-
 uid = uuid.uuid4()
 
-try:
-    connection = pymysql.connect(
-        host=host,
-        user=username,
-        password=password,
-        database=db,
-        cursorclass=pymysql.cursors.DictCursor
+def establish_connection():
+    try:
+        connection = pymysql.connect(
+            host=host,
+            user=username,
+            password=password,
+            database=db,
+            cursorclass=pymysql.cursors.DictCursor
         )
-except Exception as ex:
-    print(str(ex))
+        return connection
+    except Exception as ex:
+        print(f"Error while establishing connection: {ex}")
+        return None
 
-def reg(email,password):
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    with connection.cursor() as cursor:
+connection = establish_connection()
+
+def reg(email, password):
+    global connection
+    if connection is None:
+        connection = establish_connection()
+    if connection is not None:
         try:
-            insert_query = "INSERT INTO `user` (email,password,joindate) VALUES (%s,%s,%s)"
-            cursor.execute(insert_query,(email,hashed_password,joindata))
-            connection.commit()
-            return True
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+            with connection.cursor() as cursor:
+                insert_query = "INSERT INTO `user` (email,password,joindate) VALUES (%s,%s,%s)"
+                cursor.execute(insert_query, (email, hashed_password, joindata))
+                connection.commit()
+                return True
         except Exception as ex:
-            print(f"Error:{ex}")
+            print(f"Error: {ex}")
             return False
+    else:
+        print("Connection is not established.")
+        return False
 
 def exist_email(email):
-    try:
-        with connection.cursor() as cursor:
-            # Check if the email exists in the 'user' table
-            query = "SELECT * FROM user WHERE email = %s"
-            cursor.execute(query, (email))
-            result = cursor.fetchone()
-
-            # If the result is not None, the email exists
-            if result:
-                return True
-            else:
-                return False
-
-    except Exception as ex:
-        print(str(ex))
-
-def log(email,password):
-    try:
-        with connection.cursor() as cursor:
-            # Check if the email exists in the 'user' table
-            query = "SELECT password FROM user WHERE email = %s"
-            cursor.execute(query, (email))
-            result = cursor.fetchone()
-            if result:
-                stored_hashed_password = result['password'].encode('utf-8')
-                user_input_password = password.encode('utf-8')
-
-                # Check with using bcrypt
-                if bcrypt.checkpw(user_input_password, stored_hashed_password):
+    global connection
+    if connection is None:
+        connection = establish_connection()
+    if connection is not None:
+        try:
+            with connection.cursor() as cursor:
+                query = "SELECT * FROM user WHERE email = %s"
+                cursor.execute(query, (email))
+                result = cursor.fetchone()
+                if result:
                     return True
                 else:
                     return False
-            else:
-                    # Handle the case where the email doesn't exist
-                return False
-
-    except Exception as ex:
-        print(str(ex))
-
-def add_articles(name,about,price,image,creator):
-    print('[LOG] try to save photo')
-    image.save('olmarkt/static/photo/' + str(uid)+'.png')
-    print('[LOG] photo saved')
-    with connection.cursor() as cursor:
-        try:
-            insert_query = "INSERT INTO `article` (name,uuid,about,price,creator,createdate) VALUES (%s,%s,%s,%s,%s,%s)"
-            cursor.execute(insert_query,(name,uid,about,price,creator,joindata))
-            connection.commit()
-            return True
         except Exception as ex:
-            print(f"Error:{ex}")
+            print(f"Error: {ex}")
             return False
+    else:
+        print("Connection is not established.")
+        return False
+
+def log(email, password):
+    global connection
+    if connection is None:
+        connection = establish_connection()
+    if connection is not None:
+        try:
+            with connection.cursor() as cursor:
+                query = "SELECT password FROM user WHERE email = %s"
+                cursor.execute(query, (email))
+                result = cursor.fetchone()
+                if result:
+                    stored_hashed_password = result['password'].encode('utf-8')
+                    user_input_password = password.encode('utf-8')
+                    if bcrypt.checkpw(user_input_password, stored_hashed_password):
+                        return True
+                    else:
+                        return False
+                else:
+                    return False
+        except Exception as ex:
+            print(f"Error: {ex}")
+            return False
+    else:
+        print("Connection is not established.")
+        return False
+
+def add_articles(name, about, price, image, creator):
+    global connection
+    if connection is None:
+        connection = establish_connection()
+    if connection is not None:
+        try:
+            image.save('static/photo/' + str(uid)+'.png')
+            with connection.cursor() as cursor:
+                insert_query = "INSERT INTO `article` (name,uuid,about,price,creator,createdate) VALUES (%s,%s,%s,%s,%s,%s)"
+                cursor.execute(insert_query, (name, uid, about, price, creator, joindata))
+                connection.commit()
+                return True
+        except Exception as ex:
+            print(f"Error: {ex}")
+            return False
+    else:
+        print("Connection is not established.")
+        return False
 
 def get_user_articles(email):
-    try:
-        with connection.cursor() as cursor:
-            # Check if the email exists in the 'user' table
-            query = "SELECT * FROM `article` WHERE creator = %s "
-            cursor.execute(query, (email))
-            result = cursor.fetchall()
-
-            return result
-
-    except Exception as ex:
-        print(str(ex))
+    global connection
+    if connection is None:
+        connection = establish_connection()
+    if connection is not None:
+        try:
+            with connection.cursor() as cursor:
+                query = "SELECT * FROM `article` WHERE creator = %s "
+                cursor.execute(query, (email))
+                result = cursor.fetchall()
+                return result
+        except Exception as ex:
+            print(f"Error: {ex}")
+            return None
+    else:
+        print("Connection is not established.")
+        return None
 
 def get_article(uuid):
-    try:
-        with connection.cursor() as cursor:
+    global connection
+    if connection is None:
+        connection = establish_connection()
+    if connection is not None:
+        try:
+            with connection.cursor() as cursor:
+                query = "SELECT * FROM `article` WHERE uuid = %s "
+                cursor.execute(query, (uuid))
+                result = cursor.fetchone()
+                return result
+        except Exception as ex:
+            print(f"Error: {ex}")
+            return None
+    else:
+        print("Connection is not established.")
+        return None
 
-            query = "SELECT * FROM `article` WHERE uuid = %s "
-            cursor.execute(query, (uuid))
-            result = cursor.fetchone()
-            return result
-
-    except Exception as ex:
-        print(str(ex))
-
-def get_all_articels():
-    try:
-        with connection.cursor() as cursor:
-            query = "SELECT * FROM `article`"
-            cursor.execute(query)
-            result = cursor.fetchall()
-            return result
-    except Exception as ex:
-        print(str(ex))
+def get_all_articles():
+    global connection
+    if connection is None:
+        connection = establish_connection()
+    if connection is not None:
+        try:
+            with connection.cursor() as cursor:
+                query = "SELECT * FROM `article`"
+                cursor.execute(query)
+                result = cursor.fetchall()
+                return result
+        except Exception as ex:
+            print(f"Error: {ex}")
+            return None
+    else:
+        print("Connection is not established.")
+        return None
 
 def get_articles_by_name(name):
-    try:
-        with connection.cursor() as cursor:
-            query = "SELECT * FROM `article` WHERE name LIKE %s"
-            cursor.execute(query,('%' + name + '%'))
-            result = cursor.fetchall()
-            return result
-    except Exception as ex:
-        print(str(ex))
-
-def ensure_directory_exists(directory):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    global connection
+    if connection is None:
+        connection = establish_connection()
+    if connection is not None:
+        try:
+            with connection.cursor() as cursor:
+                query = "SELECT * FROM `article` WHERE name LIKE %s"
+                cursor.execute(query, ('%' + name + '%'))
+                result = cursor.fetchall()
+                return result
+        except Exception as ex:
+            print(f"Error: {ex}")
+            return None
+    else:
+        print("Connection is not established.")
+        return None
